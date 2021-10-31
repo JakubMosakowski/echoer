8d9319174e26e128580c9998bc1e11527659e2f0
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2021-10-31 23:45:10 +0100

    Plug main view to main activity

--- app/src/main/java/com/jakmos/echoer/presentation/main/MainActivity.kt
+++ app/src/main/java/com/jakmos/echoer/presentation/main/MainActivity.kt
@@ -3,28 +3,17 @@ package com.jakmos.echoer.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import com.jakmos.echoer.presentation.common.theme.EchoerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            EchoerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            EchoerApp()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
-
