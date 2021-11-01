33fc555464f1d4fc8d262d24d22ea01d0f9feec6
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2021-11-01 13:49:26 +0100

    Add proper colors for themes

--- presentation/src/main/java/com/jakmos/echoer/presentation/common/theme/Theme.kt
+++ presentation/src/main/java/com/jakmos/echoer/presentation/common/theme/Theme.kt
@@ -6,9 +6,25 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors()
+private val DarkColorPalette = darkColors(
+    primary = colorPrimaryDark,
+    primaryVariant = colorPrimary,
+    onPrimary = colorTextIcons,
+    secondary = colorAccent,
+    onSecondary = colorTextSecondary,
+    background = colorDarkBackground,
+    surface = colorDarkBackground
+)
 
-private val LightColorPalette = lightColors()
+private val LightColorPalette = lightColors(
+    primary = colorPrimary,
+    primaryVariant = colorPrimaryDark,
+    onPrimary = colorTextIcons,
+    secondary = colorAccent,
+    onSecondary = colorTextSecondary,
+    surface = colorTextIcons,
+    onSurface = colorTextPrimary,
+)
 
 @Composable
 fun EchoerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
