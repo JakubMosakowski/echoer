59d53c2a9a1a0d0e7024461ab61439e950ad9c4f
jakubmosakowski <jmosakowski@spoton.com>
2022-03-17 20:56:23 +0100

    Add tests for auth repository impl

--- data/src/main/kotlin/com/jakmos/echoer/data/auth/AuthRepositoryImpl.kt
+++ data/src/main/kotlin/com/jakmos/echoer/data/auth/AuthRepositoryImpl.kt
@@ -5,7 +5,6 @@ import com.jakmos.echoer.domain.auth.AuthRepository
 import com.jakmos.echoer.domain.common.Email
 import com.jakmos.echoer.domain.common.Password
 import kotlinx.coroutines.tasks.await
-import timber.log.Timber
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
@@ -13,12 +12,10 @@ class AuthRepositoryImpl @Inject constructor(
 ) : AuthRepository {
 
     override suspend fun signUp(email: Email, password: Password) {
-        val result = auth.createUserWithEmailAndPassword(email.value, password.value).await()
-        Timber.v("User created successfully: ${result.user}")
+        auth.createUserWithEmailAndPassword(email.value, password.value).await()
     }
 
     override suspend fun signIn(email: Email, password: Password) {
-        val result = auth.signInWithEmailAndPassword(email.value, password.value).await()
-        Timber.v("User logged in successfully: ${result.user}")
+        auth.signInWithEmailAndPassword(email.value, password.value).await()
     }
 }
